0b6c6204abfa43dad755d3b0dc4081449e9e59cd
victoria <v.suprun1961@gmail.com>
2026-03-26 04:24:29 +0200

    add layout

--- my-react-app/src/components/Form/Form.tsx
+++ my-react-app/src/components/Form/Form.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './Form.scss';
 
-export const Form = () => {
+export const Form = (props: {createNewToDo: Function}) => {
+    const [text, setText] = useState<string>("");
+
+    const formSubmit = () => {
+        if (text) {
+            props.createNewToDo(text);
+            setText("");
+    }
+}
+
     return (
         <div className="form-wrapper">
-            <form action="#">
+            <form action="#" onSubmit={formSubmit}>
                 <label>
-                    <input type="text" />
+                    <input
+                        value={text}
+                        type="text"
+                        onChange={(event) => setText(event.target.value)} />
                     <button></button>
                 </label>
             </form>
--- my-react-app/src/components/ToDoList/ToDoList.tsx
+++ my-react-app/src/components/ToDoList/ToDoList.tsx
@@ -1,21 +1,53 @@
 import { ToDoListItem } from "./ToDoListItem/ToDoListItem";
+import type { ToDo } from "../../models/todo-items";
 import "./ToDoList.scss";
 
-export const ToDoList = () => {
+
+
+export const ToDoList = (props: {
+  todos: ToDo[];
+  updateToDo: (item: ToDo) => void;
+  deleteToDo: (item: ToDo) => void;
+}) =>
+ {
+
+    const checkedList = () => {
+        return props.todos
+                .filter((item) => !item.isDone)
+                .map((item, index) => {
+                        return (<ToDoListItem
+                            toDoItem={item}
+                            key={index}
+                            updateToDo={props.updateToDo}
+                            deleteToDo={props.deleteToDo}/>
+                        );
+                })
+    }
+
+    const unCheckedList = () => {
+        return props.todos
+                .filter((item) => item.isDone)
+                .map((item, index) => {
+                        return (<ToDoListItem
+                            toDoItem={item}
+                            key={index}
+                            updateToDo={props.updateToDo}
+                            deleteToDo={props.deleteToDo}/>
+                        );
+                })
+    }
+
     return (
         <div className="todo-container">
         <ul className="todo-list failed">
-            <ToDoListItem />
+            {
+                checkedList()
+            }
         </ul>
         <ul className="todo-list completed">
-            <ToDoListItem />
-            {/* <li className="todo-list-item__wrapper">
-                <span>Вторая задача</span>
-                <div className="todo-list-item__buttons">
-                    <button className="btn-trash"></button>
-                    <button className="btn-uncheck"></button>
-                </div>
-            </li> */}
+            {
+                unCheckedList()
+            }
         </ul>
     </div>
     );
